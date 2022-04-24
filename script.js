6ba7bdc699e342f78593cf4785fd55a7134260c6
insert funny name here <50721698+yusuksomuch@users.noreply.github.com>
2022-04-24 13:06:24 -0400

    audio startTimes, pause function
    allowed audio to start at a time that isn't the default start.
    prevented videos from starting too far ahead so they don't try to keep playing after they've ended.
    added a pause function

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function play(id) {
     var playPromise = wav.play();
     playPromise.then(_ => {
         var b = Math.random() * Math.min(wav.duration, 9);
+        wav.currentTime = Math.random() * (wav.duration - b);
         setTimeout(() => {
             wav.pause();
             wav.src = "";
@@ -40,7 +41,7 @@ function mp4(id, callback) {
     vid.paused = true;
     vid.addEventListener('loadedmetadata', function() {
         poo = (Math.random() * Math.min(vid.duration, 5));
-        this.currentTime = Math.random() * this.duration;
+        this.currentTime = Math.random() * (this.duration - poo);
     }, false);
     var promise = vid.play();
     promise.then(_ => {
@@ -86,36 +87,58 @@ var
         );
     },
     start = () => {
-        document.body.innerHTML = `<canvas style="position:fixed;top:0px;left:0px;" width="${innerWidth}" height="${innerHeight}" id="canvas"></canvas><canvas style="position:fixed;top:0px;left:0px;" width="${innerWidth}" height="${innerHeight}" id="canvas2"></canvas>`;
+        document.body.innerHTML = `<canvas style="position:fixed;top:0px;left:0px;" width="${innerWidth}" height="${innerHeight}" id="canvas"></canvas><canvas style="position:fixed;top:0px;left:0px;" width="${innerWidth}" height="${innerHeight}" id="canvas2"></canvas><canvas style="position:fixed;top:0px;left:0px;" width="${innerWidth}" height="${innerHeight}" id="canvas3"></canvas>`;
         ctx = document.getElementById("canvas").getContext("2d");
+        ctx3 = document.getElementById("canvas3").getContext("2d");
         setInterval(() => {
-            gen();
+            if (!pause) gen();
         }, 2000);
         setInterval(() => {
-            var 
-                i = 0,
-                poopoo = Math.round(Math.random() * 10);
-            while (i < poopoo) {
+            if (!pause) {
                 var 
-                    a = png(
-                        Math.floor(Math.random() * pngs)
-                    ),
-                    g = Math.random(),
-                    x = g > 0.75 ? 0 : Math.floor(Math.random() * 2) / 2 * innerWidth,
-                    y = g > 0.75 ? 0 : Math.floor(Math.random() * 2) / 2 * innerHeight,
-                    w = g > 0.75 ? innerWidth : innerWidth / 2,
-                    h = g > 0.75 ? innerHeight : innerHeight / 2;
-                a.onload = () => {
-                    ctx.drawImage(a,x,y,w,h);
+                    i = 0,
+                    poopoo = Math.round(Math.random() * 10);
+                while (i < poopoo) {
+                    var 
+                        a = png(
+                            Math.floor(Math.random() * pngs)
+                        ),
+                        g = Math.random(),
+                        x = g > 0.75 ? 0 : Math.floor(Math.random() * 2) / 2 * innerWidth,
+                        y = g > 0.75 ? 0 : Math.floor(Math.random() * 2) / 2 * innerHeight,
+                        w = g > 0.75 ? innerWidth : innerWidth / 2,
+                        h = g > 0.75 ? innerHeight : innerHeight / 2;
+                    a.onload = () => {
+                        ctx.drawImage(a,x,y,w,h);
+                    }
+                    i++;
                 }
-                i++;
-            }
-        }, 250);
-        ctx2 = document.getElementById("canvas2").getContext("2d");
-        (anus = function () {
-            mp4(Math.floor(Math.random() * mp4s), anus);
-        })();
-    };
+            }, 250);
+            ctx2 = document.getElementById("canvas2").getContext("2d");
+            (anus = function () {
+                if (!pause) {
+                    mp4(Math.floor(Math.random() * mp4s), anus);
+                }
+            })();
+            setInterval(() => {
+                if (pause) {
+                    ctx3.font = "Arial 20px";
+                    ctx3.fillStyle = "#000000";
+                    ctx3.fillRect(0,0,innerWidth,innerHeight);
+                    ctx3.drawText("Paused", 0, innerHeight);
+                } else {
+                    ctx3.clearRect(0,0,innerWidth,innerHeight);
+                }
+            });
+        }
+    },
+    space = 0,
+    pause = false;
+document.onkeyup = (e) => {
+    if (e.key == "Space") {
+        pause = pause ? false : true;
+    }
+}
 document.body.innerHTML = `
 <h1>21funny: a humour generation engine</h1>
 <p>Generates humour (21st century guranteed!) using a bunch of preset assets. It freely mixes these with no regard for anything but being as surprising as possible (to maximize funnyness)</p>
